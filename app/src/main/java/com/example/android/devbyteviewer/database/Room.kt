aa7799cff07b39a188cbf76a48bbb9e6906b4cd4
wolsz <wolsz1@gmail.com>
2020-12-16 21:48:52 -0500

    commit

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,11 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
 
 @Dao
 interface VideoDao {
@@ -32,6 +32,23 @@ interface VideoDao {
     fun insertAll(vararg videos: DatabaseVideo)
 }
 
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+@InternalCoroutinesApi
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java, "videos").build()
+        }
+    }
+    return INSTANCE
+}
 // TODO (01) Create an abstract VideosDatabase class that extends RoomDatabase.
 
 // TODO (02) Annotate VideosDatabase with @Database,including entities and version.
